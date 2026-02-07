eb8b5ce0991d9e7751e113bd0c3c34aba81105b4
RENAN029 <pires.s@escolar.ifrn.edu.br>
2026-02-07 15:54:41 -0300

    Update D.sh

--- D.sh
+++ D.sh
@@ -22,7 +22,7 @@ cleanup_files() {
 
 de_gnome_installer() {
     local state_file="$STATE_DIR/de_gnome"
-    local pkg_gnome="gdm3 gnome-shell gnome-initial-setup gnome-console gnome-software gnome-tweaks gnome-disk-utility gnome-backgrounds"
+    local pkg_gnome="gdm3 gnome-initial-setup gnome-console gnome-software gnome-tweaks gnome-disk-utility gnome-backgrounds"
 
     if [ -f "$state_file" ] || pacman -Q gnome-shell &>/dev/null; then
         if confirm "Gnome detectado. Desinstalar?"; then
@@ -45,7 +45,7 @@ de_gnome_installer() {
 
 de_plasma_installer() {
     local state_file="$STATE_DIR/de_plasma"
-    local pkg_plasma="plasma-meta konsole dolphin kdeconnect partitionmanager ark"
+    local pkg_plasma="sddm-kcm kde-plasma-desktop konsole dolphin kdeconnect partitionmanager ark"
 
     if [ -f "$state_file" ] || pacman -Q plasma-meta &>/dev/null; then
         if confirm "Plasma detectado. Desinstalar?"; then
@@ -74,17 +74,7 @@ main_menu() {
         echo "2) Devs"
         echo "3) Drivers"
         echo "4) Educação"
-        echo "5) Extras"
-        echo "6) IDEs"
-        echo "7) Jogos"
-        echo "8) Office"
-        echo "9) Periféricos"
-        echo "10) Pessoal"
-        echo "11) Privacidade"
-        echo "12) Repositórios"
-        echo "13) Social"
-        echo "14) Utilidades"
-        echo "15) Sair"
+        echo "5) Sair"
         echo
         read -p "Selecione uma opção: " opcao
 
@@ -93,17 +83,7 @@ main_menu() {
             2) devs_menu ;;
             3) drivers_menu ;;
             4) educacao_menu ;;
-            5) extras_menu ;;
-            6) ides_menu ;;
-            7) jogos_menu ;;
-            8) office_menu ;;
-            9) perifericos_menu ;;
-            10) pessoal_menu ;;
-            11) privacidade_menu ;;
-            12) repositorios_menu ;;
-            13) social_menu ;;
-            14) utilidades_menu ;;
-            15) exit 0 ;;
+            5) exit 0 ;;
             *) ;;
         esac
         read -p "Pressione Enter para continuar..."
