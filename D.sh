0daa561a079d551c90602e1e3b84e01302bde837
RENAN029 <pires.s@escolar.ifrn.edu.br>
2026-02-11 08:02:41 -0300

    Update D.sh

--- D.sh
+++ D.sh
@@ -1121,3 +1121,23 @@ chaotic_aur_installer() {
         fi
     fi
 }
+No arquivo sources.list por pagrao(estou utilizando o debian testing) vem escrito isso apos a instalacao do debian:
+#deb cdrom:[Debian GNU/Linux testing _Forky_ - Official Snapshot amd64 NETINST with firmware 20260211-09:07]/ forky contrib main non-free-firmware
+
+deb http://deb.debian.org/debian/ forky main non-free-firmware
+deb-src http://deb.debian.org/debian/ forky main non-free-firmware
+
+deb http://security.debian.org/debian-security forky-security main non-free-firmware
+deb-src http://security.debian.org/debian-security forky-security main non-free-firmware
+
+# forky-updates, to get updates before a point release is made;
+# see https://www.debian.org/doc/manuals/debian-reference/ch02.en.html#_updates_and_backports
+deb http://deb.debian.org/debian/ forky-updates main non-free-firmware
+deb-src http://deb.debian.org/debian/ forky-updates main non-free-firmware
+
+# This system was installed using small removable media
+# (e.g. netinst, live or single CD). The matching "deb cdrom"
+# entries were disabled at the end of the installation process.
+# For information about how to configure apt package sources,
+# see the sources.list(5) manual.
+
